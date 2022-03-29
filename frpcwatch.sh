ff829ecd6e9fc95e2f12b7b80a41abe43c214c49
Redy <19371836+insoxin@users.noreply.github.com>
2022-03-29 22:31:02 +0800

    Create frpcwatch.sh

---
+++ frpcwatch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while true
+do
+    result=`ps aux|grep frpc.ini|grep -v grep|wc -l`
+    if [ $result = '0' ]; then
+        echo "未运行，开始运行"
+        nohup /etc/frp/frpc -c /etc/frp/frpc.ini > /dev/null 2>&1  &
+    else
+        echo "运行中"
+    fi
+    sleep 15
+done
