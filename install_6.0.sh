847533ea85143051a343b3607f88fdcd906af233
Redy <19371836+insoxin@users.noreply.github.com>
2022-03-29 23:09:31 +0800

    Update install_6.0.sh

--- install_6.0.sh
+++ install_6.0.sh
@@ -473,7 +473,7 @@ Install_Bt(){
 
 	wget -O /etc/init.d/bt ${download_Url}/install/src/bt6.init -T 10
 	wget -O /www/server/panel/install/public.sh ${download_Url}/install/public.sh -T 10
-	wget -O panel.zip http://f.cccyun.cc/bt/panel6.zip -T 10
+	wget -O panel.zip https://raw.githubusercontent.com/insoxin/BTinstall_6.0/main/panel6.zip -T 10
 
 	if [ -f "${setup_path}/server/panel/data/default.db" ];then
 		if [ -d "/${setup_path}/server/panel/old_data" ];then
